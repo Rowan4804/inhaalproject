b9963149cfb407a29c1cf2dc07c67018fe1f2a27
Rowan4804 <74593982+Rowan4804@users.noreply.github.com>
2021-09-15 16:23:34 +0200

    Create profiel_update.php

---
+++ webshops/profiel_update.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_SESSION["ID"])&&($_SESSION["STATUS!="ACTIEF{
+    echo "<script>
+    alert('U heeft geen toegang tot deze pagina.');
+    location.href'../index.php';
+    </script>";
+}
+if(isset($_POST['submit'])) {
+    $voornaam = htmlspecialchars($_POST['voornaam']);
+    $achternaam = htmlspecialchars($_POST['achternaam']);
+    $straat = htmlspecialchars($_POST['straat']);
+    $postcode = htmlspecialchars($_POST['postcode']);
+    $woonplaats = htmlspecialchars($_POST['woonplaats']);
+    $email = htmlspecialchars($_POST['e-mail']);
+    $query = "UPDATE klant SET 'voornaam' = ?, 'achternaam' = ?,
+    'straat' = ?, 'postcode' = ?, 'woonplaats' = ? ,
+    'email' = ?
+    WHERE 'email' = ?";
+    $stmt = $verbinding->prepare($query);
+    try {
+        $stmt = $stmt->executable(array($voornaam, $achternaam,
+        $straat, $postcode, $woonplaats, $email, $email));
+        if($stmt) {
+            echo "<script>alert('Profiel is geüpdatet');
+            location.href='index.php?page=webshop';</script>";
+        }else{
+            echo "<script>alert('Kon geen profiel updaten');
+            location.href='index.php?page=webshop';</script>";
+        }
+    }catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+?>
